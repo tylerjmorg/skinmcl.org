0596f940692d213028756353b10d60802eeb1ca9
Tyler Morgan <git@tylerjm.org>
2025-05-07 23:25:19 -0600

    Add attribution to files

--- nav.js
+++ nav.js
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2025 Logan Medical Group, LLC. All rights reserved.
+//
+// Contributers
+// Tyler Morgan <git@tylerjm.org>
+//
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
