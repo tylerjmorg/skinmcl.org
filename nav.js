902dfaf7235876c4526ccf88b3fe9ef4e5e23399
Tyler Morgan <git@tylerjm.org>
2025-05-13 06:59:43 -0600

    smarter navigation

--- nav.js
+++ nav.js
@@ -7,4 +7,31 @@
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
+
+  function handleOutsideClick(event) {
+    if (!menu.contains(event.target)) {
+      menu.classList.remove('open');
+      removeListeners();
+    }
+  }
+
+  function handleEscapeKey(event) {
+    if (event.key === 'Escape') {
+      menu.classList.remove('open');
+      removeListeners();
+    }
+  }
+
+  function removeListeners() {
+    document.removeEventListener('click', handleOutsideClick);
+    document.removeEventListener('keydown', handleEscapeKey);
+  }
+
+  if (menu.classList.contains('open')) {
+    setTimeout(() => {
+      // Timeout to allow the original click to finish before listening
+      document.addEventListener('click', handleOutsideClick);
+      document.addEventListener('keydown', handleEscapeKey);
+    }, 0);
+  }
 }
